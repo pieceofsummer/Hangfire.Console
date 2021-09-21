c1f56a954a6cf29930a35bba776675448ed4c057
Mohammad Aljaber <mnajeeb@esensesoftware.com>
2021-09-21 10:10:20 +0300

    Trying toavoid extra changes caused by Indentation fixer

--- src/Hangfire.Console/Dashboard/ConsoleRenderer.cs
+++ src/Hangfire.Console/Dashboard/ConsoleRenderer.cs
@@ -8,7 +8,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading;
 
 namespace Hangfire.Console.Dashboard
 {
